1a80f6b36e2e1bb91a4b7f9f1ddfcf75978d7170
carlosmarinho <carluizfla@hotmail.com>
2019-04-23 19:01:29 -0300

    adicionado doctor e activities migrations

---
+++ database/migrations/2019_04_23_215536_create_doctors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('crm', 50);
+            $table->string('telefone', 20);
+            $table->integer('activity_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+}
