b6e481d1aac85624b17e74b69a7c130e6368a6ff
carlosmarinho <carluizfla@hotmail.com>
2019-04-23 20:30:53 -0300

    adding softdelete and fillable

--- app/Doctors.php
+++ app/Doctors.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctors extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = ['name', 'crm', 'telefone', 'activity_id'];
+    protected $dates = ['deleted_at'];
 }
