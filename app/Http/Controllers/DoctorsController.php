fe71df71f935967e9bd3163b9567884c0b353a47
carlosmarinho <carluizfla@hotmail.com>
2019-04-23 21:11:46 -0300

    added delete_at

--- app/Http/Controllers/DoctorsController.php
+++ app/Http/Controllers/DoctorsController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Doctors;
 
 class DoctorsController extends Controller
 {
-    //
+    public function index(){
+        $Doctors = Doctors::all();
+        if(!$Doctors){
+           return response()->json([
+              'message' => 'Nenhum registro encontrado',
+           ],404);
+        }
+        return response()->json($Doctors);
+    }
 }
