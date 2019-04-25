b95a97a6f2faa3c6242ee8f96fdb2b4de2bfd9cd
carlosmarinho <carluizfla@hotmail.com>
2019-04-24 22:41:15 -0300

    resolvido problema de cors na hora do cadastro

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,8 +15,9 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-	return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        return $next($request);
+        //->header('Access-Control-Allow-Origin', '*')
+        //->header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
+        //->header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
     }
 }
